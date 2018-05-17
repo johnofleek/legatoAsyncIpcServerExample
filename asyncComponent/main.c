d6266768281230ea71990f870dc06c19a74609b4
John <john@iniveo.co.uk>
2018-05-17 08:48:25 +0100

    Update main.c

--- asyncComponent/main.c
+++ asyncComponent/main.c
@@ -4,26 +4,27 @@
 /*
  * Copyright (C) 2016-2017 Renfell Engineering Pty Ltd - All Rights Reserved
  *
- * Use of this work is subject to license and solely for use with hardware
- * supplied by Renfell Engineering Pty Ltd.
- *
- * Proprietary and confidential.
- * This file or any part thereof may not be used, distributed, copied, or
- * modified for use for any purpose other than that for which it was
- * originally provided without first receiving written permission of the
- * copyright holder.
- *
- * This source code is provided 'as is' without warranty of any kind, either
- * expressed or implied by consumer legislation or otherwise, including but
- * not limited to the implied warranties of merchantability and/or fitness
- * for a particular purpose.
- *
- * The end user acknowledges that this software is a "work in progress", and
- * as such should not be relied upon in a commercial environment or other
- * situations where economic loss or harm to persons may arise due to a
- * failure to perform to the end users specific requirements.
- *
- * By use of this software you agree to the use of the above terms.
+MIT License
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
  *
  */
 
